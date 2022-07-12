305c374d9b47faeff693177b9c5f1cd1d1c9b146
Luis Augenstein <luis.augenstein@web.de>
2022-07-12 20:33:41 +0200

    experiment with webpack config

--- apps/dnd-history/custom-webpack.config.js
+++ apps/dnd-history/custom-webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-      IMG_BB_API_KEY: process.env.IMG_BB_API_KEY,
+      IMG_BB_API_KEY: JSON.stringify(process.env.IMG_BB_API_KEY),
     }),
   ],
 };
